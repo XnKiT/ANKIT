87dc7934ff99fb7b2377c4596bd55c0ec97edd8a
⚝αикιт кυмαя⚝ <98248106+xNKIT@users.noreply.github.com>
2022-06-22 02:53:02 +0530

    Create ping.py

---
+++ Ankit/plugins/ping.py
@@ -0,0 +1,22 @@
+import os
+
+from telethon import Button, events
+
+from Ankit import *
+
+IMG = os.environ.get(
+    "PING_PIC", "https://te.legra.ph/file/c9d2beabc2870756c2df8.mp4"
+)
+ms = 27
+
+ALIVE = os.environ.get(
+    "ALIVE", "@XnKiTKumMaR"
+)
+
+CAPTION = f"**꧁•⊹٭Ping٭⊹•꧂**\n\n   ⚜ {ms}\n   ⚜ ❝𝐌𝐲 𝐌𝐚𝐬𝐭𝐞𝐫❞ ~『{ALIVE}』"
+
+
+@Ankit.on(events.NewMessage(pattern="^/ping"))
+async def _(event):
+    UMM = [[Button.url("⚜ AbOuT Me ⚜", "https://xnkit.github.io/k/")]]
+    await Ankit.send_file(event.chat_id, IMG, caption=CAPTION, buttons=UMM)
